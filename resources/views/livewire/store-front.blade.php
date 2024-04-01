e193ec2c5def22144c0c4ebe37a32f69eb302318
張建文 <chang180@gmail.com>
2024-04-01 13:50:24 +0800

    episode 5 finished

---
+++ resources/views/livewire/store-front.blade.php
@@ -0,0 +1,9 @@
+<div class="grid grid-cols-4 gap-4 mt-12 text-white">
+    @foreach ($this->product as $product)
+        <div>
+            <img src="{{ $product->image->path }}" alt="">
+            <h2 class="text-lg font-medium">{{ $product->name }}</h2>
+            <span class="text-sm text-gray-300">{{ $product->price }}</span>
+        </div>
+    @endforeach
+</div>
