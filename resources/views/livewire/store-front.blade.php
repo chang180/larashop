181da6c4e461d6aac1219cba92f18fc3165c5219
CHANG180-PC\chang <chang180@gmail.com>
2024-04-08 21:13:03 +0800

    Episode 6 finished
    1. vite added hmr to refresh npm run dev
    2. moneyphp/moeny update to 4.5 to fix the bug

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-4 gap-4 mt-12 text-white">
     @foreach ($this->product as $product)
-        <div>
+        <div class="p-4 bg-black rounded-lg shadow">
             <img src="{{ $product->image->path }}" alt="">
             <h2 class="text-lg font-medium">{{ $product->name }}</h2>
             <span class="text-sm text-gray-300">{{ $product->price }}</span>
